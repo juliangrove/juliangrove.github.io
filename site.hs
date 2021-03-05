3b2de07e35b71daed69857138a7c5c75c45a362b
Julian Grove <julian.grove@gmail.com>
2021-03-05 03:28:04 +0100

    initial commit.

---
+++ site.hs
@@ -0,0 +1,39 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE
+    OverloadedStrings #-}
+
+import Hakyll
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+  match ("images/*"
+         .||. "blog/*"
+         .||. "slides/*"
+        ) $ do
+    route idRoute
+    compile copyFileCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+  match (fromList [ "blog.org"
+                  , "code.org"
+                  , "cv.org"
+                 
+                  , "dissertation.org"
+                  ,  "index.org"
+                  , "papers.org"
+                  , "slides.org"
+                  ]) $ do
+    route $ setExtension "html"
+    compile $ do
+      item1 <- pandocCompiler
+      item2 <- loadAndApplyTemplate
+               "templates/layout.html"
+               defaultContext
+               item1
+      relativizeUrls item2
+  match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
