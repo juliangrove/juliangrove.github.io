6df2fd614d81f6ba62e0cc6a8caafd0beebb9309
juliangrove <julian.grove@gmail.com>
2023-07-07 16:43:05 -0400

    Publish.

--- experiments/projection-follow-up-1/get_answers.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('projection-only/raw_results_all') as fid:
-    data = json.load(fid)
-
-with open('projection-only/answers', 'a') as the_file:
-    for i in range(0, len(data)):
-        the_file.write(data[i]['Answer'] + '\n\n')
