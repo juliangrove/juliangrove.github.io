598a4b02ef0ca234ba6bc6791f07af00c4da5d5a
juliangrove <julian.grove@gmail.com>
2024-12-22 17:00:16 -0500

    Adjectives main experiment test.

---
+++ experiments/adjectives-test/js/stims.js
@@ -0,0 +1,90 @@
+var list1 = [
+    {
+ 	list: "1",
+ 	itemNr: "1",   
+	item: "1a", 
+	fact: "Joan is a soccer player.",
+	prompt: "How likely is it that she is tall?"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "2",   
+	item: "2b",
+	fact: "Anna has a pick-up truck, which is heavy. Josie has a small hybrid car.",
+	prompt: "How likely is it that it is heavy?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "3",   
+	item: "3c",
+	fact: "Tina has a pet orangutan, which is old. Emily has a pet hamster, which is not old. Zoe has a pet dog.",
+	prompt: "How likely is it that it is old?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "4",   
+	item: "4a",
+	fact: "Fred has a dinner table.",
+	prompt: "How likely is it that it is wide?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "5",   
+	item: "5b",
+	fact: "Frank lives near a river, which is deep. Mark lives near a swimming pool.",
+	prompt: "How likely is it that it is deep?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "6",   
+	item: "6c",
+	fact: "Doug likes his local artisan coffee shop's pourover, which is expensive. Nick likes drip coffee from his next-door diner, which is not expensive. Josh likes his favorite restaurant's cappuccino.",
+	prompt: "How likely is it that it is expensive?"
+		    }
+];
+
+var list2 = [
+    {
+ 	list: "1",
+ 	itemNr: "1",   
+	item: "1a", 
+	fact: "Joan is a soccer player.",
+	prompt: "How likely is it that she is tall?"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "2",   
+	item: "2b",
+	fact: "Anna has a pick-up truck, which is heavy. Josie has a small hybrid car.",
+	prompt: "How likely is it that it is heavy?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "3",   
+	item: "3c",
+	fact: "Tina has a pet orangutan, which is old. Emily has a pet hamster, which is not old. Zoe has a pet dog.",
+	prompt: "How likely is it that it is old?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "4",   
+	item: "4a",
+	fact: "Fred has a dinner table.",
+	prompt: "How likely is it that it is wide?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "5",   
+	item: "5b",
+	fact: "Frank lives near a river, which is deep. Mark lives near a swimming pool.",
+	prompt: "How likely is it that it is deep?"
+		    },
+    {
+ 	list: "1",
+ 	itemNr: "6",   
+	item: "6c",
+	fact: "Doug likes her local artisan coffee shop's pourover, which is expensive. Nick likes drip coffee from his next-door diner, which is not expensive. Josh likes his favorite restaurant's cappuccino.",
+	prompt: "How likely is it that it is expensive?"
+		    }
+];
+
