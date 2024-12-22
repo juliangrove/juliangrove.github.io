f40c650463ef9af76c11364a6e4fd223efd16d91
juliangrove <julian.grove@gmail.com>
2024-12-22 15:44:36 -0500

    Fix directory.

---
+++ experiments/adjectives-prior/js/stims.js
@@ -0,0 +1,171 @@
+var list1 = [
+    {
+ 	list: "1",
+ 	itemNr: "1",   
+	item: "1a", 
+	fact: "Mary is a basketball player.",
+	prompt: "Guess how tall she is.",
+	lower: "0 feet",
+	upper: "7 feet"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "2",   
+	item: "2b",
+	fact: "Josie has a golf cart.",
+	prompt: "Guess how heavy it is.",
+	lower: "0 lbs",
+	upper: "10,000 lbs"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "3",   
+	item: "3c",
+	fact: "Zoe has a pet dog.",
+	prompt: "Guess how old it is.",
+	lower: "0 years",
+	upper: "100 years"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "4",   
+	item: "4a",
+	fact: "Danny has a counter for making bread.",
+	prompt: "Guess how wide it is.",
+	lower: "0 feet",
+	upper: "20 feet"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "5",   
+	item: "5b",
+	fact: "Frank lives near a swimming pool.",
+	prompt: "Guess how deep it is.",
+	lower: "0 feet",
+	upper: "100 feet"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "6",   
+	item: "6c",
+	fact: "Josh likes his favorite restaurant's cappuccino.",
+	prompt: "Guess how expensive it is.",
+	lower: "$0",
+	upper: "$20"
+    }
+];
+
+var list2 = [
+    {
+ 	list: "2",
+ 	itemNr: "1",   
+	item: "1b", 
+	fact: "Mary is a jockey.",
+	prompt: "Guess how tall she is.",
+	lower: "0 feet",
+	upper: "7 feet"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "2",   
+	item: "2c",
+	fact: "Josie has a small hybrid car.",
+	prompt: "Guess how heavy it is.",
+	lower: "0 lbs",
+	upper: "10,000 lbs"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "3",   
+	item: "3a",
+	fact: "Zoe has a pet orangutan.",
+	prompt: "Guess how old it is.",
+	lower: "0 years",
+	upper: "100 years"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "4",   
+	item: "4b",
+	fact: "Danny has a chest of drawers.",
+	prompt: "Guess how wide it is.",
+	lower: "0 feet",
+	upper: "20 feet"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "5",   
+	item: "5c",
+	fact: "Frank lives near a swimming pool.",
+	prompt: "Guess how deep it is.",
+	lower: "0 feet",
+	upper: "100 feet"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "6",   
+	item: "6a",
+	fact: "Josh likes his local artisan coffee shop's pourover made with beans roasted in-house.",
+	prompt: "Guess how expensive it is.",
+	lower: "$0",
+	upper: "$20"
+    }
+];
+
+var list2 = [
+    {
+ 	list: "2",
+ 	itemNr: "1",   
+	item: "1b", 
+	fact: "Mary is a jockey.",
+	prompt: "Guess how tall she is.",
+	lower: "0 feet",
+	upper: "7 feet"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "2",   
+	item: "2c",
+	fact: "Josie has a small hybrid car.",
+	prompt: "Guess how heavy it is.",
+	lower: "0 lbs",
+	upper: "10,000 lbs"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "3",   
+	item: "3a",
+	fact: "Zoe has a pet orangutan.",
+	prompt: "Guess how old it is.",
+	lower: "0 years",
+	upper: "100 years"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "4",   
+	item: "4b",
+	fact: "Danny has a chest of drawers.",
+	prompt: "Guess how wide it is.",
+	lower: "0 feet",
+	upper: "20 feet"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "5",   
+	item: "5c",
+	fact: "Frank lives near a swimming pool.",
+	prompt: "Guess how deep it is.",
+	lower: "0 feet",
+	upper: "100 feet"
+    },
+    {
+ 	list: "2",
+ 	itemNr: "6",   
+	item: "6a",
+	fact: "Josh likes his local artisan coffee shop's pourover made with beans roasted in-house.",
+	prompt: "Guess how expensive it is.",
+	lower: "$0",
+	upper: "$20"
+    }
+];
+
