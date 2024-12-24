234914c2786c1a976e2356d393027b9fe741f567
juliangrove <julian.grove@gmail.com>
2024-12-24 16:06:11 -0500

    Change format of items.

--- experiments/adjectives-test/js/stims.js
+++ experiments/adjectives-test/js/stims.js
@@ -3,14 +3,14 @@ var list1 = [
  	list: "1",
  	itemNr: "1",   
 	item: "1a", 
-	fact: "Susan is a basketball player. She is tall. <br><br> Mary is a soccer player.",
+	fact: "Susan is a basketball player, and is tall, while Mary is a soccer player.",
 	prompt: "How likely is it that she is tall?"
     },
     {
  	list: "1",
  	itemNr: "2",   
 	item: "2b",
-	fact: "Anna has a pick-up truck. It is heavy. <br><br> Lois has a golf cart. It is not heavy. <br><br> Josie has a small hybrid car.",
+	fact: "Anna has a pick-up truck, which is heavy, while Lois has a golf cart, which is not heavy. Meanwhile, Josie has a small hybrid car.",
 	prompt: "How likely is it that it is heavy?"
     },
     {
@@ -24,14 +24,14 @@ var list1 = [
  	list: "1",
  	itemNr: "4",   
 	item: "4a",
-	fact: "Fred has a round banquet table for twelve people. It is wide. <br><br> Danny has a dinner table.",
+	fact: "Fred has a round banquet table for twelve people, which is wide, while Danny has a dinner table.",
 	prompt: "How likely is it that it is wide?"
     },
     {
  	list: "1",
  	itemNr: "5",   
 	item: "5b",
-	fact: "Mark lives near a river. It is deep. <br><br> Adam lives near a small pond. It is not deep <br><br> Frank lives near a swimming pool.",
+	fact: "Mark lives near a river, which is deep, while Adam lives near a small pond, which is not deep. Meanwhile, Frank lives near a swimming pool.",
 	prompt: "How likely is it that it is deep?"
     },
     {
@@ -48,7 +48,7 @@ var list2 = [
  	list: "2",
  	itemNr: "1",   
 	item: "1b", 
-	fact: "Susan is a basketball player. She is tall. <br><br> Joan is a jockey. She is not tall. <br><br> Mary is a soccer player.",
+	fact: "Susan is a basketball player, and is tall, while Joan is a jockey, and is not tall. Meanwhile, Mary is a soccer player.",
 	prompt: "How likely is it that she is tall?"
     },
     {
@@ -62,14 +62,14 @@ var list2 = [
  	list: "2",
  	itemNr: "3",   
 	item: "3a",
-	fact: "Tina has a pet horse. It is old. <br><br> Zoe has a pet dog.",
+	fact: "Tina has a pet horse, which is old, while Zoe has a pet dog.",
 	prompt: "How likely is it that it is old?"
     },
     {
  	list: "2",
  	itemNr: "4",   
 	item: "4b",
-	fact: "Fred has a round banquet table for twelve people. It is wide. <br><br> Lucas has a bed-stand for his lamp. It is not wide. <br><br> Danny has a dinner table.",
+	fact: "Fred has a round banquet table for twelve people, which is wide, while Lucas has a bed-stand for his lamp, which is not wide. Meanwhile, Danny has a dinner table.",
 	prompt: "How likely is it that it is wide?"
     },
     {
@@ -83,7 +83,7 @@ var list2 = [
  	list: "2",
  	itemNr: "6",   
 	item: "6a",
-	fact: "Arnold likes his local artisan coffee shop's pourover. It is expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
+	fact: "Arnold likes his local artisan coffee shop's pourover, which is expensive, while Josh likes his favorite restaurant's cappuccino.",
 	prompt: "How likely is it that it is expensive?"
     }
 ];
@@ -100,14 +100,14 @@ var list3 = [
  	list: "3",
  	itemNr: "2",   
 	item: "2a",
-	fact: "Anna has a pick-up truck. It is heavy. <br><br> Josie has a small hybrid car.",
+	fact: "Anna has a pick-up truck, which is heavy, while Josie has a small hybrid car.",
 	prompt: "How likely is it that it is heavy?"
     },
     {
  	list: "3",
  	itemNr: "3",   
 	item: "3b",
-	fact: "Tina has a pet horse. It is old. <br><br> Ellen has a pet hamster. It is not old. <br><br> Zoe has a pet dog.",
+	fact: "Tina has a pet horse, which is old, while Ellen has a pet hamster, which is not old. Meanwhile, Zoe has a pet dog.",
 	prompt: "How likely is it that it is old?"
     },
     {
@@ -121,14 +121,14 @@ var list3 = [
  	list: "3",
  	itemNr: "5",   
 	item: "5a",
-	fact: "Mark lives near a river. It is deep. <br><br> Frank lives near a swimming pool.",
+	fact: "Mark lives near a river, which is deep, while Frank lives near a swimming pool.",
 	prompt: "How likely is it that it is deep?"
     },
     {
  	list: "3",
  	itemNr: "6",   
 	item: "6b",
-	fact: "Arnold likes his local artisan coffee shop's pourover. It is expensive. <br><br> John likes the cup of drip coffee from his next-door diner. It is not expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
+	fact: "Arnold likes his local artisan coffee shop's pourover, which is expensive, while Nick likes the cup of drip coffee from his next-door diner, which is not expensive. Meanwhile, Josh likes his favorite restaurant's cappuccino.",
 	prompt: "How likely is it that it is expensive?"
     }
 ];
