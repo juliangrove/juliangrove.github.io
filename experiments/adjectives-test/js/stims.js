75a8e705d47e3373dfbd6c5fbffe5825204f6f17
juliangrove <julian.grove@gmail.com>
2024-12-24 14:14:45 -0500

    Edit stimuli; update instructions page.

--- experiments/adjectives-test/js/stims.js
+++ experiments/adjectives-test/js/stims.js
@@ -3,44 +3,44 @@ var list1 = [
  	list: "1",
  	itemNr: "1",   
 	item: "1a", 
-	fact: "Joan is a soccer player.",
+	fact: "Susan is a basketball player - she is tall. <br><br> Mary is a soccer player.",
 	prompt: "How likely is it that she is tall?"
     },
     {
  	list: "1",
  	itemNr: "2",   
 	item: "2b",
-	fact: "Anna has a pick-up truck, which is heavy. <br> Louis has a golf cart, which is not heavy. <br><br> Josie has a small hybrid car.",
+	fact: "Anna has a pick-up truck - it is heavy. <br><br> Lois has a golf cart - it is not heavy. <br><br> Josie has a small hybrid car.",
 	prompt: "How likely is it that it is heavy?"
     },
     {
  	list: "1",
  	itemNr: "3",   
 	item: "3c",
-	fact: "Tina has a pet orangutan, which is old. <br> Emily has a pet hamster, which is not old. <br><br> Zoe has a pet dog.",
+	fact: "Zoe has a pet dog.",
 	prompt: "How likely is it that it is old?"
     },
     {
  	list: "1",
  	itemNr: "4",   
 	item: "4a",
-	fact: "Fred has a dinner table.",
+	fact: "Fred has a round banquet table for twelve people - it is wide. <br><br> Danny has a dinner table.",
 	prompt: "How likely is it that it is wide?"
     },
     {
  	list: "1",
  	itemNr: "5",   
 	item: "5b",
-	fact: "Frank lives near a river, which is deep. <br><br> Mark lives near a swimming pool.",
+	fact: "Mark lives near a river - it is deep. <br><br> Adam lives near a small pond - it is not deep <br><br> Frank lives near a swimming pool.",
 	prompt: "How likely is it that it is deep?"
     },
     {
  	list: "1",
  	itemNr: "6",   
 	item: "6c",
-	fact: "Doug likes his local artisan coffee shop's pourover, which is expensive. <br> Nick likes drip coffee from his next-door diner, which is not expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
+	fact: "Josh likes his favorite restaurant's cappuccino.",
 	prompt: "How likely is it that it is expensive?"
-		    }
+    }
 ];
 
 var list2 = [
@@ -48,87 +48,87 @@ var list2 = [
  	list: "2",
  	itemNr: "1",   
 	item: "1b", 
-	fact: "Mary is a basketball player and is tall. <br><br> Joan is a soccer player.",
+	fact: "Susan is a basketball player - she is tall. <br><br> Joan is a jockey, and is not tall. <br><br> Mary is a soccer player.",
 	prompt: "How likely is it that she is tall?"
     },
     {
  	list: "2",
  	itemNr: "2",   
 	item: "2c",
-	fact: "Anna has a pick-up truck, which is heavy. <br><br> Josie has a small hybrid car.",
+	fact: "Josie has a small hybrid car.",
 	prompt: "How likely is it that it is heavy?"
-		    },
+    },
     {
  	list: "2",
  	itemNr: "3",   
-	item: "3c",
-	fact: "Tina has a pet orangutan, which is old. <br> Emily has a pet hamster, which is not old. <br><br> Zoe has a pet dog.",
+	item: "3a",
+	fact: "Tina has a pet horse - it is old. <br><br> Zoe has a pet dog.",
 	prompt: "How likely is it that it is old?"
-		    },
+    },
     {
  	list: "2",
  	itemNr: "4",   
-	item: "4a",
-	fact: "Fred has a dinner table.",
+	item: "4b",
+	fact: "Fred has a round banquet table for twelve people - it is wide. <br><br> Lucas has a bed-stand for his lamp - it is not wide. <br><br> Danny has a dinner table.",
 	prompt: "How likely is it that it is wide?"
-		    },
+    },
     {
  	list: "2",
  	itemNr: "5",   
-	item: "5b",
-	fact: "Frank lives near a river, which is deep. <br><br> Mark lives near a swimming pool.",
+	item: "5c",
+	fact: "Frank lives near a swimming pool.",
 	prompt: "How likely is it that it is deep?"
-		    },
+    },
     {
  	list: "2",
  	itemNr: "6",   
-	item: "6c",
-	fact: "Doug likes his local artisan coffee shop's pourover, which is expensive. <br> Nick likes drip coffee from his next-door diner, which is not expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
+	item: "6a",
+	fact: "Arnold likes his local artisan coffee shop's pourover - it is expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
 	prompt: "How likely is it that it is expensive?"
-		    }
+    }
 ];
 
 var list3 = [
     {
  	list: "3",
  	itemNr: "1",   
-	item: "1a", 
-	fact: "Joan is a soccer player.",
+	item: "1c", 
+	fact: "Mary is a soccer player.",
 	prompt: "How likely is it that she is tall?"
     },
     {
  	list: "3",
  	itemNr: "2",   
-	item: "2b",
-	fact: "Anna has a pick-up truck, which is heavy. <br><br> Josie has a small hybrid car.",
+	item: "2a",
+	fact: "Anna has a pick-up truck - it is heavy. <br><br> Josie has a small hybrid car.",
 	prompt: "How likely is it that it is heavy?"
     },
     {
  	list: "3",
  	itemNr: "3",   
-	item: "3c",
-	fact: "Tina has a pet orangutan, which is old. <br> Emily has a pet hamster, which is not old. <br><br> Zoe has a pet dog.",
+	item: "3b",
+	fact: "Tina has a pet horse - it is old. <br><br> Ellen has a pet hamster - it is not old. <br><br> Zoe has a pet dog.",
 	prompt: "How likely is it that it is old?"
-		    },
+    },
     {
  	list: "3",
  	itemNr: "4",   
-	item: "4a",
-	fact: "Fred has a dinner table.",
+	item: "4c",
+	fact: "Danny has a dinner table.",
 	prompt: "How likely is it that it is wide?"
-		    },
+    },
     {
  	list: "3",
  	itemNr: "5",   
-	item: "5b",
-	fact: "Frank lives near a river, which is deep. <br><br> Mark lives near a swimming pool.",
+	item: "5a",
+	fact: "Mark lives near a river - it is deep. <br><br> Frank lives near a swimming pool.",
 	prompt: "How likely is it that it is deep?"
-		    },
+    },
     {
  	list: "3",
  	itemNr: "6",   
-	item: "6c",
-	fact: "Doug likes his local artisan coffee shop's pourover, which is expensive. <br> Nick likes drip coffee from his next-door diner, which is not expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
+	item: "6b",
+	fact: "Arnold likes his local artisan coffee shop's pourover - it is expensive. <br><br> John likes the cup of drip coffee from his next-door diner - it is not expensive. <br><br> Josh likes his favorite restaurant's cappuccino.",
 	prompt: "How likely is it that it is expensive?"
-		    }
+    }
 ];
