68c2166953a3b091a8a9ba291447c454bfd680e9
juliangrove <julian.grove@gmail.com>
2025-03-07 13:15:38 -0500

    XPrag norming experiment.

---
+++ experiments/xprag2025/norming/js/stims.js
@@ -0,0 +1,404 @@
+var list1 = [
+    {
+ 	list: "1",
+ 	itemNr: "1",
+        scaleType: "relative",   
+	item: "1_high", 
+	fact: "Mary is a basketball player.",
+	prompt: "Guess how tall she is.",
+        adjective: "tall",
+	lower: "a coffee mug",
+	upper: "a giraffe"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "2",
+        scaleType: "relative",   
+	item: "2_low",
+	fact: "Josie has a golf cart.",
+	prompt: "Guess how heavy it is.",
+        adjective: "heavy",
+	lower: "a paperclip",
+	upper: "a cruise ship"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "3",
+        scaleType: "relative",   
+	item: "3_mid",
+	fact: "Zoe has a pet dog.",
+	prompt: "Guess how old it is.",
+        adjective: "old",
+	lower: "a newly bloomed flower",
+	upper: "a giant tortoise"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "4",
+        scaleType: "relative",   
+	item: "4_high",
+	fact: "Danny has a round banquet table for twelve people.",
+	prompt: "Guess how wide it is.",
+        adjective: "wide",
+	lower: "a drinking straw",
+	upper: "a road"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "5",
+        scaleType: "relative",   
+	item: "5_low",
+	fact: "Frank lives near a small pond.",
+	prompt: "Guess how deep it is.",
+        adjective: "deep",
+	lower: "a shallow dish",
+	upper: "the Pacific Ocean"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "6",
+        scaleType: "relative",   
+	item: "6_mid",
+	fact: "Josh likes his favorite restaurant's cappuccino.",
+	prompt: "Guess how expensive it is.",
+        adjective: "expensive",
+	lower: "a piece of candy",
+	upper: "a meal at a fancy restaurant"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "7",
+        scaleType: "absolute",   
+	item: "7_high",
+	fact: "Anna just filled up her van's tank with gas before a long road trip.",
+	prompt: "Guess how full it is.",
+        adjective: "full",
+	lower: "an empty bucket",
+	upper: "an overflowing cup"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "8",
+        scaleType: "absolute",
+	item: "8_low",
+	fact: "Liz used a sponge to wipe the counter.",
+	prompt: "Guess how dry it is.",
+        adjective: "dry",
+	lower: "a soaked raincoat",
+	upper: "a stale cracker"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "9",
+        scaleType: "absolute",
+	item: "9_mid",
+	fact: "Nancy's car has an electric engine.",
+	prompt: "Guess how quiet it is.",
+        adjective: "quiet",
+	lower: "a jet engine",
+	upper: "a sealed, soundproof room"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "10",
+        scaleType: "absolute",
+	item: "10_high",
+	fact: "Arthur bought a bottle of distilled water",
+	prompt: "Guess how pure it is.",
+        adjective: "pure",
+	lower: "a muddy puddle",
+	upper: "100% concencrated oxygen"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "11",
+        scaleType: "absolute",
+	item: "11_low",
+	fact: "Ted needs to wash his kitchen counter.",
+	prompt: "Guess how clean it is.",
+        adjective: "",
+	lower: "a garbage dump",
+	upper: "a brand new whiteboard"
+    },
+    {
+ 	list: "1",
+ 	itemNr: "12",
+        scaleType: "absolute",
+	item: "12_mid",
+	fact: "Doug has a window in his kitchen.",
+	prompt: "Guess how closed it is.",
+        adjective: "closed",
+	lower: "an open umbrella",
+	upper: "a sealed vault"
+    }
+];
+
+var list2 = [
+    {
+        list: "2",
+        itemNr: "1",
+        scaleType: "relative",   
+        item: "1_low", 
+        fact: "Mary is a jockey.",
+        prompt: "Guess how tall she is.",
+        adjective: "tall",
+        lower: "a coffee mug",
+        upper: "a giraffe"
+    },
+    {
+        list: "2",
+        itemNr: "2",
+        scaleType: "relative",   
+        item: "2_mid",
+        fact: "Josie has a small hybrid car.",
+        prompt: "Guess how heavy it is.",
+        adjective: "heavy",
+        lower: "a paperclip",
+        upper: "a cruise ship"
+    },
+    {
+        list: "2",
+        itemNr: "3",
+        scaleType: "relative",   
+        item: "3_high",
+        fact: "Zoe has a pet horse.",
+        prompt: "Guess how old it is.",
+        adjective: "old",
+        lower: "a newly bloomed flower",
+        upper: "a giant tortoise"
+    },
+    {
+        list: "2",
+        itemNr: "4",
+        scaleType: "relative",   
+        item: "4_low",
+        fact: "Danny has a bed-stand to hold his lamp.",
+        prompt: "Guess how wide it is.",
+        adjective: "wide",
+        lower: "a drinking straw",
+        upper: "a road"
+    },
+    {
+        list: "2",
+        itemNr: "5",
+        scaleType: "relative",   
+        item: "5_mid",
+        fact: "Frank lives near a swimming pool.",
+        prompt: "Guess how deep it is.",
+        adjective: "deep",
+        lower: "a shallow dish",
+        upper: "the Pacific Ocean"
+    },
+    {
+        list: "2",
+        itemNr: "6",
+        scaleType: "relative",   
+        item: "6_high",
+        fact: "Josh likes his local artisan coffee shop's pourover.",
+        prompt: "Guess how expensive it is.",
+        adjective: "expensive",
+        lower: "a piece of candy",
+        upper: "a meal at a fancy restaurant"
+    },
+    {
+        list: "2",
+        itemNr: "7",
+        scaleType: "absolute",   
+        item: "7_low",
+        fact: "Anna had a glass of red wine with dinner.",
+        prompt: "Guess how full it was.",
+        adjective: "full",
+        lower: "an empty bucket",
+        upper: "an overflowing cup"
+    },
+    {
+        list: "2",
+        itemNr: "8",
+        scaleType: "absolute",
+        item: "8_mid",
+        fact: "Liz has a leaf that she picked up outside during a windy autumn day.",
+        prompt: "Guess how dry it is.",
+        adjective: "dry",
+        lower: "a soaked raincoat",
+        upper: "a stale cracker"
+    },
+    {
+        list: "2",
+        itemNr: "9",
+        scaleType: "absolute",
+        item: "9_high",
+        fact: "Nancy is working on the lowest no-talking floor of a university library.",
+        prompt: "Guess how quiet it is.",
+        adjective: "quiet",
+        lower: "a jet engine",
+        upper: "a sealed, soundproof room"
+    },
+    {
+        list: "2",
+        itemNr: "10",
+        scaleType: "absolute",
+        item: "10_low",
+        fact: "Arthur bought a necklace made out of gold.",
+        prompt: "Guess how pure it is.",
+        adjective: "pure",
+        lower: "a muddy puddle",
+        upper: "100% concentrated oxygen"
+    },
+    {
+        list: "2",
+        itemNr: "11",
+        scaleType: "absolute",
+        item: "11_mid",
+        fact: "Ted swept his garage for an hour.",
+        prompt: "Guess how clean it is.",
+        adjective: "clean",
+        lower: "a garbage dump",
+        upper: "a brand new whiteboard"
+    },
+    {
+        list: "2",
+        itemNr: "12",
+        scaleType: "absolute",
+        item: "12_high",
+        fact: "Doug slammed the door shut.",
+        prompt: "Guess how closed it is.",
+        adjective: "closed",
+        lower: "an open umbrella",
+        upper: "a sealed vault"
+    }
+];
+
+var list3 = [
+    {
+        list: "3",
+        itemNr: "1",
+        scaleType: "relative",   
+        item: "1_mid", 
+        fact: "Mary is a soccer player.",
+        prompt: "Guess how tall she is.",
+        adjective: "tall",
+        lower: "a coffee mug",
+        upper: "a giraffe"
+    },
+    {
+        list: "3",
+        itemNr: "2",
+        scaleType: "relative",   
+        item: "2_high",
+        fact: "Josie has a pick-up truck.",
+        prompt: "Guess how heavy it is.",
+        adjective: "heavy",
+        lower: "a paperclip",
+        upper: "a cruise ship"
+    },
+    {
+        list: "3",
+        itemNr: "3",
+        scaleType: "relative",   
+        item: "3_low",
+        fact: "Zoe has a pet hamster.",
+        prompt: "Guess how old it is.",
+        adjective: "old",
+        lower: "a newly bloomed flower",
+        upper: "a giant tortoise"
+    },
+    {
+        list: "3",
+        itemNr: "4",
+        scaleType: "relative",   
+        item: "4_mid",
+        fact: "Danny has a dinner table for his family of four.",
+        prompt: "Guess how wide it is.",
+        adjective: "wide",
+        lower: "a drinking straw",
+        upper: "a road"
+    },
+    {
+        list: "3",
+        itemNr: "5",
+        scaleType: "relative",   
+        item: "5_high",
+        fact: "Frank lives near a river.",
+        prompt: "Guess how deep it is.",
+        adjective: "deep",
+        lower: "a shallow dish",
+        upper: "the Pacific Ocean"
+    },
+    {
+        list: "3",
+        itemNr: "6",
+        scaleType: "relative",   
+        item: "6_low",
+        fact: "Josh likes the cup of drip coffee from his next-door diner.",
+        prompt: "Guess how expensive it is.",
+        adjective: "expensive",
+        lower: "a piece of candy",
+        upper: "a meal at a fancy restaurant"
+    },
+    {
+        list: "3",
+        itemNr: "7",
+        scaleType: "absolute",   
+        item: "7_mid",
+        fact: "Anna had a glass of water with dinner.",
+        prompt: "Guess how full it was.",
+        adjective: "full",
+        lower: "an empty bucket",
+        upper: "an overflowing cup"
+    },
+    {
+        list: "3",
+        itemNr: "8",
+        scaleType: "absolute",
+        item: "8_high",
+        fact: "Liz just took her towel out of the dryer.",
+        prompt: "Guess how dry it is.",
+        adjective: "dry",
+        lower: "a soaked raincoat",
+        upper: "a stale cracker"
+    },
+    {
+        list: "3",
+        itemNr: "9",
+        scaleType: "absolute",
+        item: "9_low",
+        fact: "Nancy is working in a coffee shop.",
+        prompt: "Guess how quiet it is.",
+        adjective: "quiet",
+        lower: "a jet engine",
+        upper: "a sealed, soundproof room"
+    },
+    {
+        list: "3",
+        itemNr: "10",
+        scaleType: "absolute",
+        item: "10_mid",
+        fact: "Arthur bought local organic honey from the farmer's market.",
+        prompt: "Guess how pure it is.",
+        adjective: "pure",
+        lower: "a muddy puddle",
+        upper: "100% concentrated oxygen"
+    },
+    {
+        list: "3",
+        itemNr: "11",
+        scaleType: "absolute",
+        item: "11_high",
+        fact: "Ted just cleaned the hospital's operating room for a scheduled surgery.",
+        prompt: "Guess how clean it is.",
+        adjective: "clean",
+        lower: "a garbage dump",
+        upper: "a brand new whiteboard"
+    },
+    {
+        list: "3",
+        itemNr: "12",
+        scaleType: "absolute",
+        item: "12_low",
+        fact: "Doug has a book drawer next to his bed.",
+        prompt: "Guess how closed it is.",
+        adjective: "closed",
+        lower: "an open umbrella",
+        upper: "a sealed vault"
+    }
+];
