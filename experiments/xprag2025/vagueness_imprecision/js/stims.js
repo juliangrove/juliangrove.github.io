acb256035a943e300ccabc90ace0069fc456ca98
juliangrove <julian.grove@gmail.com>
2025-03-09 17:45:40 -0400

    Add vagueness_imprecision experiment.

---
+++ experiments/xprag2025/vagueness_imprecision/js/stims.js
@@ -0,0 +1,332 @@
+var lst1 = [
+    {
+ 	list: "1",
+ 	item_nr: "1",
+        scale_type: "relative",   
+	item: "1_high", 
+	fact: "Mary is a basketball player.",
+	prompt: "How likely is it that she is tall?",
+        adjective: "tall"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "2",
+        scale_type: "relative",   
+	item: "2_low",
+	fact: "Josie has a golf cart.",
+	prompt: "How likely is it that it is heavy?",
+        adjective: "heavy"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "3",
+        scale_type: "relative",   
+	item: "3_mid",
+	fact: "Zoe has a pet dog.",
+	prompt: "How likely is it that it is old?",
+        adjective: "old"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "4",
+        scale_type: "relative",   
+	item: "4_high",
+	fact: "Danny has a round banquet table for twelve people.",
+	prompt: "How likely is it that it is wide?",
+        adjective: "wide"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "5",
+        scale_type: "relative",   
+	item: "5_low",
+	fact: "Frank lives near a small pond.",
+	prompt: "How likely is it that it is deep?",
+        adjective: "deep"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "6",
+        scale_type: "relative",   
+	item: "6_mid",
+	fact: "Josh likes his favorite restaurant's cappuccino.",
+	prompt: "How likely is it that it is expensive?",
+        adjective: "expensive"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "7",
+        scale_type: "absolute",   
+	item: "7_high",
+	fact: "Anna just filled up her van's tank with gas before a long road trip.",
+	prompt: "How likely is it that it is full?",
+        adjective: "full"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "8",
+        scale_type: "absolute",
+	item: "8_low",
+	fact: "Liz used a sponge to wipe the counter.",
+	prompt: "How likely is it that it is dry?",
+        adjective: "dry"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "9",
+        scale_type: "absolute",
+	item: "9_mid",
+	fact: "Nancy's car has an electric engine.",
+	prompt: "How likely is it that it is quiet?",
+        adjective: "quiet"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "10",
+        scale_type: "absolute",
+	item: "10_high",
+	fact: "Arthur bought a bottle of distilled water",
+	prompt: "How likely is it that it is pure?",
+        adjective: "pure"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "11",
+        scale_type: "absolute",
+	item: "11_low",
+	fact: "Ted needs to wash his kitchen counter.",
+	prompt: "How likely is it that it is clean?",
+        adjective: "clean"
+    },
+    {
+ 	list: "1",
+ 	item_nr: "12",
+        scale_type: "absolute",
+	item: "12_mid",
+	fact: "Doug has a window in his kitchen.",
+	prompt: "How likely is it that it is closed?",
+        adjective: "closed"
+    }
+];
+
+var list2 = [
+    {
+        list: "2",
+        item_nr: "1",
+        scale_type: "relative",   
+        item: "1_low", 
+        fact: "Mary is a jockey.",
+        prompt: "How likely is it that she is tall?",
+        adjective: "tall"
+    },
+    {
+        list: "2",
+        item_nr: "2",
+        scale_type: "relative",   
+        item: "2_mid",
+        fact: "Josie has a small hybrid car.",
+        prompt: "How likely is it that it is heavy?",
+        adjective: "heavy"
+    },
+    {
+        list: "2",
+        item_nr: "3",
+        scale_type: "relative",   
+        item: "3_high",
+        fact: "Zoe has a pet horse.",
+        prompt: "How likely is it that it is old?",
+        adjective: "old"
+    },
+    {
+        list: "2",
+        item_nr: "4",
+        scale_type: "relative",   
+        item: "4_low",
+        fact: "Danny has a bed-stand to hold his lamp.",
+        prompt: "How likely is it that it is wide?",
+        adjective: "wide"
+    },
+    {
+        list: "2",
+        item_nr: "5",
+        scale_type: "relative",   
+        item: "5_mid",
+        fact: "Frank lives near a swimming pool.",
+        prompt: "How likely is it that it is deep?",
+        adjective: "deep"
+    },
+    {
+        list: "2",
+        item_nr: "6",
+        scale_type: "relative",   
+        item: "6_high",
+        fact: "Josh likes his local artisan coffee shop's pourover.",
+        prompt: "How likely is it that it is expensive?",
+        adjective: "expensive"
+    },
+    {
+        list: "2",
+        item_nr: "7",
+        scale_type: "absolute",   
+        item: "7_low",
+        fact: "Anna had a glass of red wine with dinner.",
+        prompt: "How likely is it that it was full?",
+        adjective: "full"
+    },
+    {
+        list: "2",
+        item_nr: "8",
+        scale_type: "absolute",
+        item: "8_mid",
+        fact: "Liz has a leaf that she picked up outside during a windy autumn day.",
+        prompt: "How likely is it that it is dry?",
+        adjective: "dry"
+    },
+    {
+        list: "2",
+        item_nr: "9",
+        scale_type: "absolute",
+        item: "9_high",
+        fact: "Nancy is working on the lowest no-talking floor of a university library.",
+        prompt: "How likely is it that it is quiet?",
+        adjective: "quiet"
+    },
+    {
+        list: "2",
+        item_nr: "10",
+        scale_type: "absolute",
+        item: "10_low",
+        fact: "Arthur bought a necklace made out of gold.",
+        prompt: "How likely is it that it is pure?",
+        adjective: "pure"
+    },
+    {
+        list: "2",
+        item_nr: "11",
+        scale_type: "absolute",
+        item: "11_mid",
+        fact: "Ted swept his garage for an hour.",
+        prompt: "How likely is it that it is clean?",
+        adjective: "clean"
+    },
+    {
+        list: "2",
+        item_nr: "12",
+        scale_type: "absolute",
+        item: "12_high",
+        fact: "Doug slammed the door shut.",
+        prompt: "How likely is it that it is closed?",
+        adjective: "closed"
+    }
+];
+
+var list3 = [
+    {
+        list: "3",
+        item_nr: "1",
+        scale_type: "relative",   
+        item: "1_mid", 
+        fact: "Mary is a soccer player.",
+        prompt: "How likely is it that she is tall?",
+        adjective: "tall"
+    },
+    {
+        list: "3",
+        item_nr: "2",
+        scale_type: "relative",   
+        item: "2_high",
+        fact: "Josie has a pick-up truck.",
+        prompt: "How likely is it that it is heavy?",
+        adjective: "heavy"
+    },
+    {
+        list: "3",
+        item_nr: "3",
+        scale_type: "relative",   
+        item: "3_low",
+        fact: "Zoe has a pet hamster.",
+        prompt: "How likely is it that it is old?",
+        adjective: "old"
+    },
+    {
+        list: "3",
+        item_nr: "4",
+        scale_type: "relative",   
+        item: "4_mid",
+        fact: "Danny has a dinner table for his family of four.",
+        prompt: "How likely is it that it is wide?",
+        adjective: "wide"
+    },
+    {
+        list: "3",
+        item_nr: "5",
+        scale_type: "relative",   
+        item: "5_high",
+        fact: "Frank lives near a river.",
+        prompt: "How likely is it that it is deep?",
+        adjective: "deep"
+    },
+    {
+        list: "3",
+        item_nr: "6",
+        scale_type: "relative",   
+        item: "6_low",
+        fact: "Josh likes the cup of drip coffee from his next-door diner.",
+        prompt: "How likely is it that it is expensive?",
+        adjective: "expensive"
+    },
+    {
+        list: "3",
+        item_nr: "7",
+        scale_type: "absolute",   
+        item: "7_mid",
+        fact: "Anna had a glass of water with dinner.",
+        prompt: "How likely is it that it was full?",
+        adjective: "full"
+    },
+    {
+        list: "3",
+        item_nr: "8",
+        scale_type: "absolute",
+        item: "8_high",
+        fact: "Liz just took her towel out of the dryer.",
+        prompt: "How likely is it that it is dry?",
+        adjective: "dry"
+    },
+    {
+        list: "3",
+        item_nr: "9",
+        scale_type: "absolute",
+        item: "9_low",
+        fact: "Nancy is working in a coffee shop.",
+        prompt: "How likely is it that it is quiet?",
+        adjective: "quiet"
+    },
+    {
+        list: "3",
+        item_nr: "10",
+        scale_type: "absolute",
+        item: "10_mid",
+        fact: "Arthur bought local organic honey from the farmer's market.",
+        prompt: "How likely is it that it is pure?",
+        adjective: "pure"
+    },
+    {
+        list: "3",
+        item_nr: "11",
+        scale_type: "absolute",
+        item: "11_high",
+        fact: "Ted just cleaned the hospital's operating room for a scheduled surgery.",
+        prompt: "How likely is it that it is clean?",
+        adjective: "clean"
+    },
+    {
+        list: "3",
+        item_nr: "12",
+        scale_type: "absolute",
+        item: "12_low",
+        fact: "Doug has a book drawer next to his bed.",
+        prompt: "How likely is it that it is closed?",
+        adjective: "closed"
+    }
+];
